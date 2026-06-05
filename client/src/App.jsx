742bc4d38a1a66710936b2cd07f55eadf2e23d70
zen2511 <ndawakainanajoseph2511@gmail.cm>
2026-06-05 14:11:17 +0200

    J1 - Init structure /client et /server complets

---
+++ client/src/App.jsx
@@ -0,0 +1,1 @@
+function App() { return <div></div> } export default App
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors    = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({ message: 'Serveur OK' });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
